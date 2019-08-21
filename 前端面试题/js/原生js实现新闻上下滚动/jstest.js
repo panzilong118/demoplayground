ec92d5575a16f2e7533f3fe3f68b2351e732c858
panzilong <panzilong2006@126.com>
2019-08-21 11:07:56 +0800

    JavaScript实现新闻上下滚动 change

--- 前端面试题/js/原生js实现新闻上下滚动/jstest.js
+++ 前端面试题/js/原生js实现新闻上下滚动/jstest.js
@@ -1,12 +1,8 @@
 var n=document.getElementById("news");
 var p1=document.getElementById("p1");
-var p2=document.getElementById("p2");
-p2.innerHTML=p1.innerHTML;
-//alert(n.offsetHeight+" 1  "+p1.offsetHeight);
 var f=function(){
     n.scrollTop++;
     if(n.scrollTop>=p1.offsetHeight){
-        //alert(n.scrollTop+" 1  "+p1.offsetHeight);
         n.scrollTop=0;
     }
 }
