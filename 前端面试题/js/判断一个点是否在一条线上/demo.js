7462bffb86da711b9e6232fa9b561bf47948fe0f
panzilong <panzilong2006@126.com>
2019-09-01 21:58:59 +0800

    实现一个方法getKeys not finished 2

--- 前端面试题/js/判断一个点是否在一条线上/demo.js
+++ 前端面试题/js/判断一个点是否在一条线上/demo.js
@@ -1,30 +1,71 @@
 /*
-* 判断一个点是否在一条线上
-* https://stackoverflow.com/questions/17692922/check-is-a-point-x-y-is-between-two-points-drawn-on-a-straight-line
-* if (distance(A, C) + distance(B, C) == distance(A, B))
-*    return true; // C is on the line.
-*    return false; // C is not on the line.
+* 实现一个方法getKeys(data, str);
+* getKeys(data, 'str1');
+* 返回: 'key1'
+* getKeys(data, 'str3');
+* 返回: 'key2, key3'
+* getKeys(data, 'str6');
+* 返回: 'key2, key5, key6'
+*
+* Tencent interview question, fulfill getKeys(data, str) function using JS.
+* get the key and all parents keys. can you help me?
 */
 
-const A = {
-  x: 1,
-  y: 1
+// traverse tree(json), fulfill getKeys(data, str) function using JS.
+// get the key and all parents keys.
+//
+// const data = {
+//   key1: 'str1',
+//   key2: {
+//     key3: 'str3',
+//     key4: 'str4',
+//     key5: {
+//       key6: 'str6',
+//       key7: 'str7',
+//       key8: 'str8',
+//     },
+//   }
+// }
+//
+// for example:
+// getKeys(data, 'str1');
+// return: 'key1'
+// getKeys(data, 'str3');
+// return: 'key2, key3'
+// getKeys(data, 'str6');
+// return: 'key2, key5, key6'
+//
+// I think it can be done be recursion, but how?
+
+const data = {
+  key1: 'str1',
+  key2: {
+    key3: 'str3',
+    key4: 'str4',
+    key5: {
+      key6: 'str6',
+      key7: 'str7',
+      key8: 'str8',
+    },
+  }
+}
+let s = [];
+function getKeys(data, str, key='') {
+  if (key !== '') {
+    s.push(key);
+  }
+  for (item in data) {
+    if (typeof data[item] === 'object') {
+      getKeys(data[item], str, item);
+    } else if (data[item] === str) {
+      s.push(item);
+      return s;
+    }
+  }
+  return s;
 }
 
-const B = {
-  x: 10,
-  y: 10
-}
-
-const C = {
-  x: 3,
-  y: 3
-}
-
-function distance(start, end){
-  const x = Math.pow((start.x - end.x), 2);
-  const y = Math.pow((start.y - end.y), 2);
-  return Math.sqrt(x + y);
-}
-
-return distance(A, C) + distance(B, C) === distance(A, B);
+// const keys = getKeys(data, 'str1');
+const keys = getKeys(data, 'str3');
+// const keys = getKeys(data, 'str6');
+// const keys = getKeys(data, 'str2');
